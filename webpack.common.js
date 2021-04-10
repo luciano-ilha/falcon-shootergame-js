65bea173cda5840982da1def4752852570cdd139
raisework <ilha.dev.br@gmail.com>
2021-04-10 17:55:24 -0300

    Create js files and add some images for testing

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/game.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),

ddc6153535712ce75adedf6df35da3c06c16a823
raisework <ilha.dev.br@gmail.com>
2021-04-10 16:44:29 -0300

    Install babel and phaser dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },

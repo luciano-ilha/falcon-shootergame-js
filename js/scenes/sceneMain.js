85029a510a364e679949b774415801c825847f0a
raisework <ilha.dev.br@gmail.com>
2021-04-15 15:30:50 -0300

    Add player ship movement and interactiveness possibilities

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -19,6 +19,23 @@ class SceneMain extends Phaser.Scene {
     this.background = this.add.image(0, 0, "background");
     this.background.setOrigin(0, 0);
     this.ship = this.physics.add.sprite(this.centerX, this.centerY, "ship");
+
+    this.background.setInteractive();
+    this.background.on("pointerdown", this.backgroundClicked, this);
+
+    // this.cursors = this.input.keyboard.createCursorKeys();
   }
-  update() {}
+
+  backgroundClicked() {
+    let tx = this.background.input.localX;
+    let ty = this.background.input.localY;
+
+    this.physics.moveTo(this.ship, tx, ty, 60);
+  }
+
+  update() {
+    // if (this.cursors.down.isDown) {
+    //   this.ship.y += 1;
+    // }
+  }
 }

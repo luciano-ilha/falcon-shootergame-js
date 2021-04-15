5fd9a4e3e2e4f51104dd2d24b91f62a44ea56eb7
raisework <ilha.dev.br@gmail.com>
2021-04-15 16:00:02 -0300

    Change ship movement to keyboard, commented click option

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -20,22 +20,60 @@ class SceneMain extends Phaser.Scene {
     this.background.setOrigin(0, 0);
     this.ship = this.physics.add.sprite(this.centerX, this.centerY, "ship");
 
-    this.background.setInteractive();
-    this.background.on("pointerdown", this.backgroundClicked, this);
+    // this.background.setInteractive();
+    // this.background.on("pointerdown", this.backgroundClicked, this);
 
-    // this.cursors = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  backgroundClicked() {
-    let tx = this.background.input.localX;
-    let ty = this.background.input.localY;
+  // backgroundClicked() {
+  //   let tx = this.background.input.localX;
+  //   let ty = this.background.input.localY;
 
-    this.physics.moveTo(this.ship, tx, ty, 60);
+  //   let angle = this.physics.moveTo(this.ship, tx, ty, 60);
+  //   angle = this.toDegrees(angle);
+  //   this.ship.angle = angle;
+  // }
+
+  toDegrees(angle) {
+    return angle * (180 / Math.PI);
   }
 
   update() {
-    // if (this.cursors.down.isDown) {
-    //   this.ship.y += 1;
-    // }
+    if (this.cursors.left.isDown) {
+      this.ship.x -= 2;
+      this.ship.angle = -180;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.ship.y -= 2;
+      this.ship.angle = -90;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.ship.x += 2;
+      this.ship.angle = 0;
+    }
+
+    if (this.cursors.down.isDown) {
+      this.ship.y += 2;
+      this.ship.angle = 90;
+    }
+
+    if (this.cursors.left.isDown && this.cursors.up.isDown) {
+      this.ship.angle = -135;
+    }
+
+    if (this.cursors.right.isDown && this.cursors.up.isDown) {
+      this.ship.angle = -45;
+    }
+
+    if (this.cursors.right.isDown && this.cursors.down.isDown) {
+      this.ship.angle = 45;
+    }
+
+    if (this.cursors.left.isDown && this.cursors.down.isDown) {
+      this.ship.angle = 135;
+    }
   }
 }

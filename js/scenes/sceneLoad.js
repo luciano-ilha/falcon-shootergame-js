eb5be148b241025091125a039e73eb53f0a3ad6d
raisework <ilha.dev.br@gmail.com>
2021-04-17 14:21:50 -0300

    Add title scene

--- js/scenes/sceneLoad.js
+++ js/scenes/sceneLoad.js
@@ -19,7 +19,7 @@ class SceneLoad extends Phaser.Scene {
     this.progText.setOrigin(0.5, 0.5);
     this.load.on("progress", this.onProgress, this);
 
-    this.load.image("button1", "/images/ui/buttons/2/1.png");
+    this.load.image("button1", "/images/ui/buttons/2/4.png");
     this.load.image("button2", "/images/ui/buttons/2/5.png");
 
     this.load.image("toggleBack", "/images/ui/toggles/1.png");
--- js/scenes/sceneTitle.js
+++ js/scenes/sceneTitle.js
@@ -4,7 +4,6 @@ class SceneTitle extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("button1", "/images/ui/buttons/2/1.png");
     this.load.image("title", "/images/title.png");
   }
 
@@ -15,20 +14,20 @@ class SceneTitle extends Phaser.Scene {
     this.alignGrid = new AlignGrid({ rows: 11, cols: 11, scene: this });
     // this.alignGrid.showNumbers();
 
-    // this.backImage = this.add.image(
-    //   game.config.width / 2,
-    //   game.config.height / 2,
-    //   "titleBack"
-    // );
+    this.add.image(0, 0, "background").setOrigin(0.5, 0.5);
 
     let title = this.add.image(0, 0, "title");
     Align.scaleToGameW(title, 0.8);
-    this.alignGrid.placeAtIndex(38, title);
+    this.alignGrid.placeAtIndex(27, title);
+    let ship = this.add.image(0, 0, "ship");
+    this.alignGrid.placeAtIndex(60, ship);
+    ship.angle = 270;
+    Align.scaleToGameW(ship, 0.18);
 
     let btnStart = new FlatButton({
       scene: this,
       key: "button1",
-      text: "start",
+      text: "Start",
       event: "start_game",
     });
     this.alignGrid.placeAtIndex(93, btnStart);
@@ -36,7 +35,6 @@ class SceneTitle extends Phaser.Scene {
     emitter.on("start_game", this.startGame, this);
     mediaManager = new MediaManager({ scene: this });
     // mediaManager.setBackgroundMusic("backgroundMusic");
-    // this.scene.start("SceneMain");
   }
 
   startGame() {

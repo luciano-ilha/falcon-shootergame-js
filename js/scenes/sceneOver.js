4061603f95460b8eb13cf523b289e62b4290e7af
raisework <ilha.dev.br@gmail.com>
2021-04-17 13:48:33 -0300

    Add winner in game over scene

--- js/scenes/sceneOver.js
+++ js/scenes/sceneOver.js
@@ -10,11 +10,24 @@ class SceneOver extends Phaser.Scene {
 
   create() {
     this.alignGrid = new AlignGrid({ rows: 11, cols: 11, scene: this });
-    // this.alignGrid.showNumbers();
+    this.alignGrid.showNumbers();
 
     let title = this.add.image(0, 0, "title");
     Align.scaleToGameW(title, 0.8);
-    this.alignGrid.placeAtIndex(38, title);
+    this.alignGrid.placeAtIndex(16, title);
+
+    this.winnerText = this.add.text(0, 0, "WINNER", {
+      fontSize: game.config.width / 10,
+      color: "#3fe213",
+    });
+    this.winnerText.setOrigin(0.5, 0.5);
+    this.alignGrid.placeAtIndex(38, this.winnerText);
+
+    if (model.playerWon == true) {
+      this.winner = this.add.image(0, 0, "ship");
+    } else {
+      this.winner = this.add.image(0, 0, "eship");
+    }
 
     let btnStart = new FlatButton({
       scene: this,
@@ -23,6 +36,9 @@ class SceneOver extends Phaser.Scene {
       event: "start_game",
     });
     this.alignGrid.placeAtIndex(93, btnStart);
+    Align.scaleToGameW(this.winner, 0.25);
+    this.winner.angle = 270;
+    this.alignGrid.placeAtIndex(60, this.winner);
 
     emitter.on("start_game", this.startGame, this);
   }
--- js/scenes/sceneTitle.js
+++ js/scenes/sceneTitle.js
@@ -36,7 +36,7 @@ class SceneTitle extends Phaser.Scene {
     emitter.on("start_game", this.startGame, this);
     mediaManager = new MediaManager({ scene: this });
     // mediaManager.setBackgroundMusic("backgroundMusic");
-    this.scene.start("SceneMain");
+    // this.scene.start("SceneMain");
   }
 
   startGame() {

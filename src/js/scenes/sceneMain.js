6b9ebdb6696edbf1ee6558e6ff0ca9fa01faf72f
raisework <ilha.dev.br@gmail.com>
2021-04-22 16:23:19 -0300

    Trying to create leaderboards and change health back

--- src/js/scenes/sceneMain.js
+++ src/js/scenes/sceneMain.js
@@ -17,8 +17,8 @@ export class SceneMain extends Phaser.Scene {
       );
     }
     // ships health
-    this.shields = 1;
-    this.eshields = 1;
+    this.shields = 150;
+    this.eshields = 100;
     // if player wins
     model.playerWon = true;
     // center screen
